8a48e2e673bc0fa3b4dfff47f961e5c575e1b47d
5abhisheksaxena <5abhisheksaxena@gmail.com>
2020-09-09 11:09:54 +0530

    Plant timber

---
+++ app/src/main/java/in/abhisheksaxena/baaten/MyApplication.kt
@@ -0,0 +1,20 @@
+package `in`.abhisheksaxena.baaten
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+
+/**
+ * @author Abhishek Saxena
+ * @since 09-09-2020 11:03
+ */
+
+@HiltAndroidApp
+class MyApplication : Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
