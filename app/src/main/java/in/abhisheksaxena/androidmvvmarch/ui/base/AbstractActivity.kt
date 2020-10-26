15253821ea80332b6d9f540cd09984807974339b
5abhisheksaxena <5abhisheksaxena@gmail.com>
2020-10-26 16:12:23 +0530

    Add data binding with nav graph.

--- app/src/main/java/in/abhisheksaxena/androidmvvmarch/ui/base/AbstractActivity.kt
+++ app/src/main/java/in/abhisheksaxena/androidmvvmarch/ui/base/AbstractActivity.kt
@@ -1,12 +1,42 @@
 package `in`.abhisheksaxena.androidmvvmarch.ui.base
 
 import `in`.abhisheksaxena.androidmvvmarch.R
+import `in`.abhisheksaxena.androidmvvmarch.databinding.ActivityAbstractBinding
 import android.os.Bundle
+import androidx.annotation.NavigationRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 
-class AbstractActivity : AppCompatActivity() {
+abstract class AbstractActivity : AppCompatActivity() {
+
+    protected lateinit var binding: ActivityAbstractBinding
+
+    @get:NavigationRes
+    abstract val graphRes: Int
+
+    protected lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_abstract)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_abstract)
+
+        initNavHostFragment(graphRes)
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
+    }
+
+    private fun initNavHostFragment(graphRes: Int) {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navInflater = navHostFragment.navController.navInflater
+        navHostFragment.navController.graph = navInflater.inflate(graphRes)
+        navController = navHostFragment.navController
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
